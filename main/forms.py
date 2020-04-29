0b412056448c19c6ef5497b06befad98abe5708b
Ricardo Escobar <ricardoescobar@Ricardos-MacBook-Pro.local>
2020-04-29 18:04:38 -0500

    Agregar primera base del proyecto

---
+++ main/forms.py
@@ -0,0 +1,62 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from main.models import User
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Usuario', 
+                            validators=[DataRequired(), Length(min=2,max=20)])
+    email = StringField('Correo',
+                            validators=[DataRequired(), Email()])
+    password = PasswordField('Contraseña', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirmar Contraseña', 
+                            validators=[DataRequired(), EqualTo('password')])
+    
+    submit = SubmitField('Registrarse')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('Ya existe una cuenta con ese nombre de usuario. Intenta con otro')
+    
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('Ya existe una cuenta con ese correo. Intenta con otro')
+
+class LoginForm(FlaskForm):
+    email = StringField('Correo Electronico',
+                            validators=[DataRequired(), Email()])
+    password = PasswordField('Contraseña', validators=[DataRequired()])
+    remember = BooleanField('Recuerdame')
+    
+    submit = SubmitField('Ingresar')
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Usuario', 
+                            validators=[DataRequired(), Length(min=2,max=20)])
+    email = StringField('Correo Electronico',
+                            validators=[DataRequired(), Email()])
+
+    picture = FileField('Actualizar Foto de Perfil', validators=[FileAllowed(['jpg', 'png'])])
+
+    submit = SubmitField('Actualizar')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('Ya existe una cuenta con ese nombre de usuario. Intenta con otro')
+    
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('Ya existe una cuenta con ese correo. Intenta con otro')
+
+class PostForm(FlaskForm):
+    title= StringField('Nombre', validators=[DataRequired()])
+    content = TextAreaField('Descripcion', validators=[DataRequired()])
+    submit = SubmitField('Registrar Evento')
---
+++ run.py
@@ -0,0 +1,10 @@
+
+
+from main import app 
+
+if __name__  == '__main__':
+    app.run(debug=True)
+
+
+
+    
